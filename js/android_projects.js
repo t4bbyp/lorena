c6520890d3a9eef753d87495b9fd83ca8b429b4e
t4bbyp <132542472+t4bbyp@users.noreply.github.com>
2025-11-17 16:08:09 +0200

    Update android_projects.js

--- js/android_projects.js
+++ js/android_projects.js
@@ -1,4 +1,4 @@
-           fetch("/../jsons/android_jsons.json")
+           fetch("../jsons/android_jsons.json")
                     .then((response) => response.json())
                     .then(data => {
                       const levels = data.levels;
